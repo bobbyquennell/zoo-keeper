32dae1a9674fb25a8f4b3c91681faed210a940b7
Bobby Wang <bobbyquennell@gmail.com>
2024-04-17 14:28:49 +1000

    organization: create OU and create new member accounts

--- organization/main.tf
+++ organization/main.tf
@@ -23,3 +23,31 @@ resource "aws_organizations_organization" "my_org" {
   feature_set                  = "ALL"
   enabled_policy_types         = ["SERVICE_CONTROL_POLICY", "TAG_POLICY"]
 }
+
+# Create the prod OU
+resource "aws_organizations_organizational_unit" "prod_ou" {
+  name = "Prod OU"
+  parent_id = aws_organizations_organization.my_org.roots[0].id
+}
+
+# Create the "dev" OU
+resource "aws_organizations_organizational_unit" "dev_ou" {
+  name = "Dev OU"
+  parent_id = aws_organizations_organization.my_org.roots[0].id
+}
+
+# Create the "prod" member account
+resource "aws_organizations_account" "prod_account" {
+  name  = var.prod_account_name
+  email = var.prod_account_email
+  role_name = "rootAccountUserAdminRole"
+  parent_id = aws_organizations_organizational_unit.prod_ou.id
+}
+
+# Create the "dev" member account
+resource "aws_organizations_account" "dev_account" {
+  name  = var.dev_account_name
+  email = var.dev_account_email
+  role_name = "rootAccountUserAdminRole"
+  parent_id = aws_organizations_organizational_unit.dev_ou.id
+}
---
+++ organization/variables.tf
@@ -0,0 +1,19 @@
+variable "prod_account_name" {
+  description = "Name of the Prod account"
+  type        = string
+}
+
+variable "prod_account_email" {
+  description = "Email address of the Prod account"
+  type        = string
+}
+
+variable "dev_account_name" {
+  description = "Name of the Dev account"
+  type        = string
+}
+
+variable "dev_account_email" {
+  description = "Email address of the Dev account"
+  type        = string
+}
